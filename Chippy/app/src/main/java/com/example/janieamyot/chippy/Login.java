ad0123915f1e577fab3e3de3a036f21bc9d2963d
Jon Oke <joke087@uottawa.ca>
2018-10-22 15:24:59 -0400

    Updated MainActivity and Login java files. Added functions to buttons in layouts

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/Login.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/Login.java
@@ -1,7 +1,11 @@
 package com.example.janieamyot.chippy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+    public void OnLoginClick(View view){
+        EditText userName = (EditText) findViewById(R.id.userNameField);
+        EditText password = (EditText) findViewById(R.id.passwordField);
+        String user = userName.getText().toString();
+        String pass = password.getText().toString();
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        Account account = dbHandler.findAccountByUserName(user);
+        if (account == null){
+            userName.setText("");
+            password.setText("");
+            Toast.makeText(this, "Username not found in database", Toast.LENGTH_LONG).show();
+        }
+        else if(account.getPassword().equals(pass)) {
+            Intent intent = new Intent(getApplicationContext(), AdminWelcomePage.class);
+            startActivity(intent);
+        }
+        else{
+            userName.setText("");
+            password.setText("");
+            Toast.makeText(this, "Password is incorrect", Toast.LENGTH_LONG).show();
+        }
     }
 }
