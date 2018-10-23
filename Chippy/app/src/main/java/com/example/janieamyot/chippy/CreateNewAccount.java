8ae401601d7f3a9816d4ad08b998f7260765c9dc
Iris Abildayeva <iabil078@uottawa.ca>
2018-10-22 22:16:02 -0400

    Created home owner and service provider activities. Changed color scheme.

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/CreateNewAccount.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/CreateNewAccount.java
@@ -3,11 +3,11 @@ package com.example.janieamyot.chippy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateNewAcccount extends AppCompatActivity {
+public class CreateNewAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_acccount);
+        setContentView(R.layout.activity_create_new_account);
     }
 }
