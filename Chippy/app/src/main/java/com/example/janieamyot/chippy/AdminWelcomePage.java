9421c1ba9ca4419c0499c455d9214da75cdb2e49
Alex M <amold022@uottawa.ca>
2018-10-24 20:41:03 -0400

    logout added

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/AdminWelcomePage.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/AdminWelcomePage.java
@@ -1,13 +1,26 @@
 package com.example.janieamyot.chippy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AdminWelcomePage extends AppCompatActivity {
+
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_welcome_page);
+        bundle = savedInstanceState;
     }
+
+    public void OnClickLogOut(View view){
+
+        Intent intent = new Intent(getApplication(), MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
