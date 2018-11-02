1d67a4d35eb60fd78d01a8c02295f10c27d3a763
Iris Abildayeva <iabil078@uottawa.ca>
2018-11-02 01:41:50 -0400

    Added navigation drawer to Admin page.

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/AdminWelcomePage.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/AdminWelcomePage.java
@@ -2,22 +2,52 @@ package com.example.janieamyot.chippy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class AdminWelcomePage extends AppCompatActivity {
 
     Bundle bundle;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_welcome_page);
+        setContentView(R.layout.activity_admin_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        return true;
+                    }
+                }
+        );
+
         Intent intent = this.getIntent();
         bundle = intent.getExtras();
         TextView welcome = findViewById(R.id.welcomeMessage);
@@ -28,6 +58,16 @@ public class AdminWelcomePage extends AppCompatActivity {
         final ArrayList<String> list = displayAccounts();
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
         accountList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private ArrayList<String> displayAccounts(){
