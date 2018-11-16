d4a5edf16ce1e7e685d26a3d3b3c7af5ea8f7e42
Iris Abildayeva <iabil078@uottawa.ca>
2018-11-16 13:54:44 -0500

    Designed layout for service provider profile. Added logic to populate layout with user information.

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/SPProfileFragment.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/SPProfileFragment.java
@@ -23,12 +23,38 @@ public class SPProfileFragment extends Fragment{
         bundle = intent.getExtras();
 
         TextView welcome = getView().findViewById(R.id.welcomeMessage);
-        String welcomeMessage = "Welcome " + extractAccount() + ", logged as Service Provider.";
+        String welcomeMessage = "Welcome " + extractAccount()[2] + ", logged as Service Provider.";
         welcome.setText(welcomeMessage);
+
+        TextView name = getView().findViewById(R.id.spNameField);
+        name.setText(extractAccount()[0]);
+        TextView lastName = getView().findViewById(R.id.spLastNameField);
+        lastName.setText(extractAccount()[1]);
+        TextView userName = getView().findViewById(R.id.spUserNameField);
+        userName.setText(extractAccount()[2]);
+        TextView email = getView().findViewById(R.id.spEmailField);
+        email.setText(extractAccount()[3]);
+        TextView address = getView().findViewById(R.id.spAddressField);
+        address.setText(extractAccount()[4]);
+        TextView phone = getView().findViewById(R.id.spPhoneField);
+        phone.setText(extractAccount()[5]);
+        TextView description = getView().findViewById(R.id.spDescriptionField);
+        description.setText(extractAccount()[6]);
+        TextView licensed = getView().findViewById(R.id.spLicensedField);
+        licensed.setText(extractAccount()[7]);
     }
 
-    private String extractAccount(){
+    private String[] extractAccount(){
         ServiceProvider serviceProvider = (ServiceProvider) bundle.get("Account");
-        return serviceProvider.getUserName();
+        String[] str = new String[8];
+        str[0] = serviceProvider.getName();
+        str[1] = serviceProvider.getLastName();
+        str[2] = serviceProvider.getUserName();
+        str[3] = serviceProvider.getEmail();
+        //str[4] = serviceProvider.getAddress();
+        //str[5] = serviceProvider.getPhone();
+        //str[6] = serviceProvider.getDescription();
+        //str[7] = serviceProvider.getLicensed();
+        return str;
     }
 }
