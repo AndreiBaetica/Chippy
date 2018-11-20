d90c1ddd8cc48fe2e41b899a4571a3beb7b1734e
unknown <andrei.baetic@gmail.com>
2018-11-20 15:58:27 -0500

    Added availability display

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/SPAvailableFragment.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/SPAvailableFragment.java
@@ -11,7 +11,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class SPAvailableFragment extends Fragment {
 
@@ -35,23 +40,114 @@ public class SPAvailableFragment extends Fragment {
         final ArrayList<String> listAvailabilities = displayAvailabilities();
         ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, listAvailabilities);
         availableList.setAdapter(adapter);
-        final MyDBHandler dbHandler = new MyDBHandler(getActivity());
-
-        availableList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO: ADD LOGIC HERE
-
-            }
-        });
     }
 
 
 
-    private ArrayList<String> displayAvailabilities(){
-        // TODO: ADD LOGIC HERE
+    private ArrayList<String> displayAvailabilities() {
+
         ArrayList<String> listAvailabilities = new ArrayList<String>();
 
+        ServiceProvider account = (ServiceProvider) bundle.get("Account");
+        JSONObject week = null;
+        try {
+            week = new JSONObject(account.getAvailabilities());
+
+        ArrayList<Integer[]> monday = sequentialSplit(week.getJSONArray("monday"));
+        String mo = "Monday:\n    ";
+        for (Integer[] array : monday) {
+            int start = array[0];
+            int end = array[array.length];
+            mo = mo.concat(start + ":00-" + end + ":00\n    ");
+        }
+        listAvailabilities.add(mo);
+
+        ArrayList<Integer[]> tuesday = sequentialSplit(week.getJSONArray("tuesday"));
+            String tu = "Tuesday:\n    ";
+            for (Integer[] array : tuesday) {
+                int start = array[0];
+                int end = array[array.length];
+                tu = tu.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(tu);
+
+        ArrayList<Integer[]> wednesday = sequentialSplit(week.getJSONArray("wednesday"));
+            String we = "Wednesday:\n    ";
+            for (Integer[] array : wednesday) {
+                int start = array[0];
+                int end = array[array.length];
+                we = we.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(we);
+
+        ArrayList<Integer[]> thursday = sequentialSplit(week.getJSONArray("thursday"));
+            String th = "Thursday:\n    ";
+            for (Integer[] array : thursday) {
+                int start = array[0];
+                int end = array[array.length];
+                th = th.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(th);
+
+        ArrayList<Integer[]> friday = sequentialSplit(week.getJSONArray("friday"));
+            String fr = "Friday:\n    ";
+            for (Integer[] array : friday) {
+                int start = array[0];
+                int end = array[array.length];
+                fr = fr.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(fr);
+
+        ArrayList<Integer[]> saturday = sequentialSplit(week.getJSONArray("saturday"));
+            String sa = "Saturday:\n    ";
+            for (Integer[] array : saturday) {
+                int start = array[0];
+                int end = array[array.length];
+                sa = sa.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(sa);
+
+        ArrayList<Integer[]> sunday = sequentialSplit(week.getJSONArray("sunday"));
+            String su = "Sunday:\n    ";
+            for (Integer[] array : sunday) {
+                int start = array[0];
+                int end = array[array.length];
+                su = su.concat(start + ":00-" + end + ":00\n    ");
+            }
+            listAvailabilities.add(su);
+
+        } catch (JSONException e) {
+            //TODO
+        }
+
         return listAvailabilities;
+    }
+
+    private ArrayList<Integer[]> sequentialSplit(JSONArray jsonArray) throws JSONException {
+        ArrayList<Integer> parsingList = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            parsingList.add(jsonArray.getInt(i));
+        }
+
+        Integer[] dayArray = (Integer[]) parsingList.toArray();
+
+        ArrayList<Integer[]> mainList = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        temp.add(dayArray[0]);
+
+        for (int i = 0; i < dayArray.length -1; i++) {
+            if (dayArray[i+1] == dayArray[i] + 1) {
+                temp.add(dayArray[i+1]);
+            } else {
+                Integer[] tempArray = (Integer[]) temp.toArray();
+                mainList.add(tempArray);
+                temp = new ArrayList<>();
+                temp.add(dayArray[i+1]);
+            }
+        }
+        Integer[] tempArray = (Integer[]) temp.toArray();
+        mainList.add(tempArray);
+        return mainList;
     }
 }
