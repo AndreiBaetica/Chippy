da5ffefc6a0ada72ee6109590495b30329d5858e
Jon Oke <joke087@uottawa.ca>
2018-12-01 15:53:27 -0500

    Completed the bookings class. Added associations between bookings ratings service providers and home owners

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/Booking.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/Booking.java
@@ -4,9 +4,38 @@ import java.util.*;
 public class Booking {
     private Calendar startTime;
     private Calendar endTime;
+    private Rating rating;
+    private int bookingId;
 
-    //TODO: determine how the dates are being manipulated elsewhere and make the constructor/getters/setters
-    public Booking (){
-
+    public Booking (Calendar startTime, Calendar endTime, int bookingId){
+        this.endTime=endTime;
+        this.startTime=startTime;
+        this.bookingId = bookingId;
+    }
+    public void setRating(Rating rating){
+        this.rating=rating;
+    }
+    public void setStartTime(int day, int hour, int minute){
+        startTime = Calendar.getInstance();
+        startTime.set(startTime.getTime().getYear(), startTime.getTime().getMonth(), day, hour, minute);
+    }
+    public void setEndTime(int day, int hour, int minute){
+        startTime = Calendar.getInstance();
+        startTime.set(startTime.getTime().getYear(), startTime.getTime().getMonth(), day, hour, minute);
+    }
+    public Calendar getStartTime(){
+        return startTime;
+    }
+    public Calendar getEndTime(){
+        return endTime;
+    }
+    public Rating getRating(){
+        return rating;
+    }
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+    public int getBookingId() {
+        return bookingId;
     }
 }
