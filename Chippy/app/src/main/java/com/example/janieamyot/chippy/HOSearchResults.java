bcffa4ab3bec533d29abfada0ea160a6a3899cd1
Iris Abildayeva <iabil078@uottawa.ca>
2018-12-03 14:58:46 -0500

    Added action bar to search results tab.

--- Chippy/app/src/main/java/com/example/janieamyot/chippy/HOSearchResults.java
+++ Chippy/app/src/main/java/com/example/janieamyot/chippy/HOSearchResults.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,6 +49,8 @@ public class HOSearchResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_ho_search_result);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         Intent intent = this.getIntent();
         bundle = intent.getExtras();
